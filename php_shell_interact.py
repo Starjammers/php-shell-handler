fc77543e58b34f99bf364ae53e21a77f6ec8c126
Mitchell Kettlewell <watchdog2000@protonmail.com>
2021-03-31 19:11:09 +0100

    rearranging and refactoring package. Also added further validation for the webshell_url to ensure a php file is actually specified

---
+++ php_shell_interact.py
@@ -0,0 +1,28 @@
+import argparse
+from php_shell_interactor.php_shell_handler import PHP_Web_Shell_Handler
+
+
+def main():
+    arg_parser = argparse.ArgumentParser(description="Python based tool to interact with php web shells!")
+    arg_parser.add_argument("-u", "--url", help=f"The URL location that your web shell will be uploaded to", required=True)
+    arg_parser.add_argument("-p", "--param", help=f"The name of the php get parameter used in the php file uploaded (default of '{PHP_Web_Shell_Handler.DEFAULT_WEBSHELL_PARAM}' is used with the built in php shells that are suggested to be used")
+    arg_parser.add_argument("--encoded", help=f"Decide whether communication with the shell will be encoded or not (default is not encoded, but add --encoded to allow encoded shells)", nargs='*')
+
+    args = arg_parser.parse_args()
+
+    url = args.url
+
+    php_param_name = None
+    if args.param:
+        php_param_name = args.param
+
+    encoded = False
+    if args.encoded != None:
+        encoded = True
+
+    handler = PHP_Web_Shell_Handler(url, encoded=encoded, webshell_param=php_param_name)    
+    handler.shell()
+
+
+if __name__ == '__main__':
+    main()
--- php_shell_interactor/php_shell_handler.py
+++ php_shell_interactor/php_shell_handler.py
@@ -1,4 +1,4 @@
-import web_utils
+from php_shell_interactor import web_utils
 import sys
 from base64 import b64encode, b64decode
 import binascii
@@ -19,8 +19,10 @@ class PHP_Web_Shell_Handler:
     def __init__(self, webshell_url, webshell_param=None, encoded=True):
         try:
             webshell_url = web_utils.validate_url(webshell_url)
-        except web_utils.InvalidUrlError:
-            print(f"[-] - The provided url: '{webshell_url} does not seem to be a valid url'")
+            if '/' not in webshell_url or not webshell_url.endswith('.php'):
+                raise web_utils.InvalidRequestError("NO FILE TO PHP FILE FOUND")
+        except web_utils.InvalidRequestError:
+            print(f"[-] - The provided url: '{webshell_url}'' does not seem to be a valid url with a php file endpoint")
             sys.exit(0)
 
         if not web_utils.connection_check(webshell_url):
@@ -177,29 +179,3 @@ class PHP_Web_Shell_Handler:
 
             else:
                 print(self._exec_cmd(cmd))
-
-
-def main():
-    arg_parser = argparse.ArgumentParser(description="Python based tool to interact with php web shells!")
-    arg_parser.add_argument("-u", "--url", help=f"The URL location that your web shell will be uploaded to", required=True)
-    arg_parser.add_argument("-p", "--param", help=f"The name of the php get parameter used in the php file uploaded (default of '{PHP_Web_Shell_Handler.DEFAULT_WEBSHELL_PARAM}' is used with the built in php shells that are suggested to be used")
-    arg_parser.add_argument("--encoded", help=f"Decide whether communication with the shell will be encoded or not (default is not encoded, but add --encoded to allow encoded shells)", nargs='*')
-
-    args = arg_parser.parse_args()
-
-    url = args.url
-
-    php_param_name = None
-    if args.param:
-        php_param_name = args.param
-
-    encoded = False
-    if args.encoded != None:
-        encoded = True
-
-    handler = PHP_Web_Shell_Handler(url, encoded=encoded, webshell_param=php_param_name)    
-    handler.shell()
-
-
-if __name__ == '__main__':
-    main()
