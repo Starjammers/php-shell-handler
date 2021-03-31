106631df2fbdae15fe26684887d207791195edf3
Mitchell Kettlewell <watchdog2000@protonmail.com>
2021-03-31 19:21:19 +0100

    added option to specify OS for C2 command execution

--- php_shell_interact.py
+++ php_shell_interact.py
@@ -7,7 +7,7 @@ def main():
     arg_parser.add_argument("-u", "--url", help=f"The URL location that your web shell will be uploaded to", required=True)
     arg_parser.add_argument("-p", "--param", help=f"The name of the php get parameter used in the php file uploaded (default of '{PHP_Web_Shell_Handler.DEFAULT_WEBSHELL_PARAM}' is used with the built in php shells that are suggested to be used")
     arg_parser.add_argument("--encoded", help=f"Decide whether communication with the shell will be encoded or not (default is not encoded, but add --encoded to allow encoded shells)", nargs='*')
-    arg_parser.add_argument("-o", "--os" help=f"Set the operating system to either WINDOWS or UNIX for C2 functionality to work correctly")
+    arg_parser.add_argument("-o", "--os", help=f"Set the operating system to either WINDOWS or UNIX for C2 functionality to work correctly")
 
     args = arg_parser.parse_args()
 
